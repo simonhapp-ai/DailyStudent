a512308dd6e3c353862c3b8c5c24bde2f391c91d
Simon <simon.happ@gmx.de>
2026-06-12 18:51:38 +0200

    fix: remove unused SUBJECT_INFO import in KlausurplanScreen

--- src/screens/KlausurplanScreen.tsx
+++ src/screens/KlausurplanScreen.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useUser } from '../context/UserContext'
-import { SUBJECT_INFO, resolveSubjectInfo, getTopicPlaceholder } from '../data/subjectInfo'
+import { resolveSubjectInfo, getTopicPlaceholder } from '../data/subjectInfo'
 import { topics } from '../data/mockData'
 
 function toDateStr(d: Date): string {
